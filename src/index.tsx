ec707ae3a4ec9f4540a1820d01542ec9a9bc01d9
marlon-erler <viridian035@gmail.com>
2024-10-22 20:21:19 +0200

    reflect theme changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import "@pwa-fundament/stylesheet/index.css";
-
-import { Theme, setTheme } from "@pwa-fundament/themes";
+import "@pwa-fundament/themes/themes/standard"
 
 import ViewRoot from "./Main/viewRoot";
 
 // prepare
 document.title = "PWA Fundament";
-setTheme(Theme.Standard);
 
 // build UI
 document.body.append(ViewRoot());
