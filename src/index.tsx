5677825cafce26d4d259212616cd1c4bc6f3d4db
marlon-erler <viridian035@gmail.com>
2024-11-29 12:08:49 +0000

    import setTheme method

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import "@pwa-fundament/stylesheet/index.css";
-import "@pwa-fundament/themes/themes/standard"
+import "@pwa-fundament/themes/themes/standard";
+
+import { setTheme } from "@pwa-fundament/themes";
 
 import ViewRoot from "./Main/viewRoot";
 
 // prepare
 document.title = "PWA Fundament";
+setTheme("standard");
 
 // build UI
 document.body.append(ViewRoot());
